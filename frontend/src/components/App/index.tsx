1be16b751e9cfd89cf69d9209792633ff8f99487
DioMain <dima123.zample@gmail.com>
2024-10-15 20:55:58 +0300

    fix: test video player

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
 const App: React.FC = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <header>
+        <video src="/videos/template/video.mp4" controls width={512}></video>
+      </header>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
