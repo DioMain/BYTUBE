9fec49bd79233bdda4016bec3505241da0ae78eb
DioMain <dima123.zample@gmail.com>
2024-11-12 12:19:08 +0300

    feat: overview video

--- frontend/src/components/StudioPage/VideosSettings/VideoOverview/index.tsx
+++ frontend/src/components/StudioPage/VideosSettings/VideoOverview/index.tsx
@@ -3,12 +3,13 @@ import VideoModel from "@type/models/VideoModel";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import QueriesUrls from "@helpers/QeuriesUrls";
 import { useStores } from "appStoreContext";
-import { IconButton, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
+import VideoItem from "./VideoItem";
+import UploadIcon from "@mui/icons-material/UploadFile";
 
 import "./style.scss";
-import VideosOverview from "./VideoOverview";
 
-const VideosSettings: React.FC = () => {
+const VideosOverview: React.FC = () => {
   const [videos, setVideos] = useState<VideoModel[]>([]);
 
   const { channel } = useStores();
@@ -29,10 +30,18 @@ const VideosSettings: React.FC = () => {
   }, [channel.value]);
 
   return (
-    <Stack className="studiovideowrapper" spacing={3}>
-      <VideosOverview />
+    <Stack className="studio-videooverview" spacing={3}>
+      <Stack className="studio-videooverview__upload" direction={"row"} justifyContent={"center"} spacing={2}>
+        <h3>Загрузить видео</h3>
+        <UploadIcon />
+      </Stack>
+      <Stack className="studio-videooverview-videos" spacing={2}>
+        {videos.map((video, index) => {
+          return <VideoItem video={video} key={`vi${index}`} />;
+        })}
+      </Stack>
     </Stack>
   );
 };
 
-export default VideosSettings;
+export default VideosOverview;
