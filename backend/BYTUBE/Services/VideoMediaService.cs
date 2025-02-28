3f514e8e46b759d4aacd3e202ab1d1d21a798d1b
DioMain <dima123.zample@gmail.com>
2025-02-28 16:43:30 +0300

    feat: added compresing

--- backend/BYTUBE/Services/VideoMediaService.cs
+++ backend/BYTUBE/Services/VideoMediaService.cs
@@ -1,17 +1,44 @@
-﻿using Xabe.FFmpeg;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Diagnostics;
+using Xabe.FFmpeg;
 
 namespace BYTUBE.Services
 {
     public class VideoMediaService
     {
+        private readonly string ffmpegPath;
+
         public VideoMediaService(string ffmpegPath)
         {
+            this.ffmpegPath = ffmpegPath;
+
             FFmpeg.SetExecutablesPath(ffmpegPath);
         }
 
         public async Task<IMediaInfo> GetMediaInfo(string videoPath)
         {
             return await FFmpeg.GetMediaInfo(videoPath);
+        }
+
+        public async Task CompressVideo(string inputPath, string outputPath, string resolution)
+        {
+            var args = $"-i \"{inputPath}\" -vf scale={resolution} -c:v libx264 -crf 28 -preset fast -c:a aac \"{outputPath}\"";
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = ffmpegPath,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            await process.WaitForExitAsync();
         }
     }
 }
